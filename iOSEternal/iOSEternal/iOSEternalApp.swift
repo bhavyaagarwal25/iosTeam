6945e28dafc67eddf5c9750b44f277d8d5370ed4
Shubh Singhal <your-github-email@example.com>
2026-07-18 16:56:31 +0530

    helloShubh

---
+++ iOSEternal/iOSEternal/iOSEternalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSEternalApp.swift
+//  iOSEternal
+//
+//  Created by Shubh Singhal on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSEternalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
